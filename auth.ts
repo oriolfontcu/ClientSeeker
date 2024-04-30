4393109929d780fad69e442618adb00ffaeb7b0c
Oriol Fontcuberta <contact@oriolfontcuberta.com>
2024-04-30 16:49:22 +0200

    Callbacks, OAuth Providers, and Bugs Solved

--- auth.ts
+++ auth.ts
@@ -1,10 +1,64 @@
 import NextAuth from "next-auth"
+import { UserRole, UserMembership } from "@prisma/client"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
+import { getUserById } from "@/data/user"
 import { db } from "@/lib/db"
 import authConfig from "@/auth.config"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  pages : {
+    signIn: "/auth/login",
+    error: "/auth/error"
+  },
+  events: {
+    async linkAccount({ user }){
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date()}
+      })
+    }
+  },
+  callbacks: {
+    // async signIn({ user }){
+    //   const existingUser = await getUserById(user.id!);
+
+    //   if(!existingUser || !existingUser.emailVerified){
+    //     return false
+    //   }
+
+    //   return true
+    // },
+    async session({ token, session }){
+      console.log({ sessionToken: token })
+
+      if (token.sub && session.user){
+          session.user.id = token.sub
+      }
+
+      if (token.role && session.user){
+        session.user.role = token.role as UserRole;
+      }
+
+      if (token.membership && session.user){
+        session.user.membership = token.membership as UserMembership
+      }
+
+      return session;
+    },
+    async jwt({ token }){
+      if (!token.sub) return token
+
+      const existingUser = await getUserById(token.sub);
+
+      if(!existingUser) return token
+      
+      token.role = existingUser.role
+      token.membership = existingUser.membership;
+
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
