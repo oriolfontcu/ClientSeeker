22e35c33d3138a0158f85b5f93195c855920595a
Oriol <oriolfontcu@gmail.com>
2024-06-07 00:11:29 -0300

    Lading Started, Login Bugs to Solve!

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const font = Poppins({
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col w-screen h-screen bg-background">
+      <main className="flex flex-col w-screen h-screen bg-secondary">
         <NavbarLanding />
         <HeaderLanding />
       </main>
